22287c6b7bfc5894159a8380ae29ce0797a8d711
mariela <36265002+mariela27@users.noreply.github.com>
2018-04-12 12:30:04 -0300

    Add files via upload

---
+++ Clase_05/vectores/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10//DIRECTIVA DEL PRECOMPILADOR //DEFINIDO Y CONSTANTE
+/*vectores (direccion de memoria del primer vector) se puede cargar por extenion
+ solo se puede hacer al momento de declarar la variable
+
+
+int main()
+{
+
+    int vector[TAM];//{9,-3,5,14,-4};//20 byte en memoria
+    int i=0;
+   // printf("vector %d\n", vector);//solo poner el nombre de la variable en este caso es vector
+
+   for(i=0; i<TAM; i++)
+   {
+       vector[i]=0;
+   }
+   /*carga secuencial indica pararme en cada elemento ingresar un valor para cada vector
+   no puedo saltearme ningun espacio
+   for(i=0; i<TAM; i++)
+   {
+       printf("ingrese un numero:");
+       scanf("%d", &vector[i]);
+   }
+
+   /*aleatoria que el usuario decida donde guardar el dato*/
+   /*c trabaja cerca de la memoria ram(hardware)
+    for(i=0; i<TAM; i++)
+    {
+        printf("%d--%d\n",i, vector[i]);
+    }
+
+    return 0;
+}
+*/
+/*
+void mostrarVector(int[], int);
+void modificarValor(int[], int);
+int main()
+{
+    int vector[TAM];//{9,-3,5,14,-4};//20 byte en memoria
+    int i=0;
+
+    for(i=0; i<TAM; i++)
+   {
+       vector[i]=0;
+   }
+    for(i=0; i<TAM; i++)
+   {
+       printf("ingrese un numero:");
+       scanf("%d", &vector[i]);
+   }
+   mostrarVector(vector; TAM);
+   modificarValor(vector);
+
+   return 0;
+}
+void mostrarVector(int vec[], int tamanio)
+{
+    int i;
+
+    for(i=0; i<tamanio; i++)
+    {
+        printf("%d--%d\n",i, vector[i]);
+    }
+}
+void modificarValor(int [], int)
+{
+
+}
+}
+*/
+//FUNCION QUE BUSQUE EL VECTOR Y DEVUELVA EL MAXIMO VALOR
+int buscarMaximo(int[], int);
+int main()
+{
+    int i;
+    int max;
+    int flag=0;
+    for(i=0; i<TAM; i++)
+    {
+
+    }
+    for(i=0; i<TAM; i++)
+    {
+
+    }
+    return 0;
+}
+
+int buscarMaximo(int vec[], int TAM)
+{
+    int i;
+    int max;
+    int flag=0;
+    for(i=0; i<TAM; i++)
+    {
+        if (flag==|| vec[])
+    }
+}
+//NUMEROS POSITIVOS BUSCAR PARES ARRAY DE ENTEROS
